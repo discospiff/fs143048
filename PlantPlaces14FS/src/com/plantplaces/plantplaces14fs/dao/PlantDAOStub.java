c6a4e850974a4c3ac2c99ae74f923e5db005d15e
jonesbr <jonesbr@ucmail.uc.edu>
2014-09-11 20:13:55 -0400

    Make our stub more realistic by filtering by the search term.

--- PlantPlaces14FS/src/com/plantplaces/plantplaces14fs/dao/PlantDAOStub.java
+++ PlantPlaces14FS/src/com/plantplaces/plantplaces14fs/dao/PlantDAOStub.java
@@ -13,34 +13,38 @@ import com.plantplaces.plantplaces14fs.dto.Plant;
  */
 public class PlantDAOStub implements IPlantDAO {
 
-		/* (non-Javadoc)
-		 * @see com.plantplaces.plantplaces14fs.dao.IPlantDAO#fetchPlants(java.lang.String)
-		 */
-		@Override
-		public List<Plant> fetchPlants(String searchTerm) {
-			// declare our return variable.
-			List<Plant> allPlants = new ArrayList<Plant>();
-			
+	/* (non-Javadoc)
+	 * @see com.plantplaces.plantplaces14fs.dao.IPlantDAO#fetchPlants(java.lang.String)
+	 */
+	@Override
+	public List<Plant> fetchPlants(String searchTerm) {
+		// declare our return variable.
+		List<Plant> allPlants = new ArrayList<Plant>();
+
+		if (searchTerm.contains("Monkey")) {
 			// Create a monkeyPuzzle
 			Plant monkeyPuzzle = new Plant();
 			monkeyPuzzle.setGenus("Aurucaria");
 			monkeyPuzzle.setSpecies("araucana");
 			monkeyPuzzle.setCommon("Monkey Puzzle");
-			
+
 			// add to collection.
 			allPlants.add(monkeyPuzzle);
-			
+		}
+
+		if (searchTerm.contains("Redbud")) {
 			// create a Redbud.
 			Plant redbud = new Plant();
 			redbud.setGenus("Cercis");
 			redbud.setSpecies("canadensis");
 			redbud.setCommon("Redbud");
-			
+
 			// add to collection
 			allPlants.add(redbud);
-			
-			// return our return variable.
-			return allPlants;
 		}
-	
+
+		// return our return variable.
+		return allPlants;
+	}
+
 }
