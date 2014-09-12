32ce7cb97b88481ea19b3369826cee9b1948cd41
jonesbr <jonesbr@ucmail.uc.edu>
2014-09-11 20:04:18 -0400

    Create the service layer stub.

---
+++ PlantPlaces14FS/src/com/plantplaces/plantplaces14fs/service/PlantServiceStub.java
@@ -0,0 +1,33 @@
+package com.plantplaces.plantplaces14fs.service;
+
+import java.util.List;
+
+import com.plantplaces.plantplaces14fs.dao.IPlantDAO;
+import com.plantplaces.plantplaces14fs.dao.PlantDAOStub;
+import com.plantplaces.plantplaces14fs.dto.Plant;
+
+/**
+ * Business and integration logic for our plant service.
+ * @author jonesbr
+ *
+ */
+public class PlantServiceStub {
+
+	// the variable type is the interface
+	IPlantDAO plantDAO;
+	
+	public PlantServiceStub() {
+		// the object type is the implementing class.
+		plantDAO = new PlantDAOStub();
+	}
+	
+	/**
+	 * Return a list of plants that match the search term.
+	 * @param searchTerm the word that we're looking for in the plant.
+	 * @return a matching list of plants.
+	 */
+	public List<Plant> fetchPlants(String searchTerm) {
+		return plantDAO.fetchPlants(searchTerm);
+	}
+	
+}
